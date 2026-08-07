f01350f03e59b4125a530453d93439fc50d5d34f
NobleGorilla14 <K1LLT3K@hotmail.com>
2026-08-07 15:38:14 -0500

    Basic static site configured

---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+class TextType(Enum):
+    plain_text = 'text'
+    bold_text = 'bold'
+    italic_text = 'italic'
+    code_text = 'code'
+    link = 'link'
+    image = 'image'
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
